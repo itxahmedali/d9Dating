2093d95399cac2c89c9ca33121575c0d3e0aa958
saniaayoub <92912305+saniaayoub@users.noreply.github.com>
2023-05-02 06:02:27 -0700

    Final commit

--- index.js
+++ index.js
@@ -1,27 +1,82 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+const https = require('https').createServer(app);
+const cors = require('cors');
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+const socketIO = require('socket.io')(https, {
+  cors: {
+    origin: '<http://192.168.18.226:3000>',
+    methods: ['GET', 'POST'],
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+socketIO.on('connection', socket => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  const users = [];
+  for (let [id, socket] of socketIO.of('/').sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  socketIO.emit('users', users);
+  console.log(users);
+  socket.broadcast.emit('user connected', {
+    userID: socket.id,
+    username: socket.username,
   });
-
+  socket.on('private message', ({content, to, timestamp}) => {
+    console.log('sent,recieve', content, to);
+    socket.to(to).emit('private message', {
+      content,
+      from: socket.id,
+      time: `${timestamp.hour}:${timestamp.mins}`,
+    });
+  });
+ 
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    socket.disconnect();
+    console.log('🔥: A user disconnected');
   });
 });
 
-const PORT = process.env.PORT || 3000;
 
-http.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+socketIO.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  console.log(socket.handshake.auth.username, 'server');
+
+  if (!username) {
+    return next(new Error('invalid username'));
+  }
+  socket.username = username;
+  next();
+});
+
+app.get('/api', (req, res) => {
+  res.json(chatRooms);
+});
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+https.listen(port, () => {
+  console.log(`Server listening on ${port}`);
 });
