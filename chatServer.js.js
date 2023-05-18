ff626fa63d6f5c075cf59d466d1b1eacbcc41721
saniaayoub <92912305+saniaayoub@users.noreply.github.com>
2023-05-17 23:21:04 -0700

    old code

--- chatServer.js.js
+++ chatServer.js.js
@@ -1,33 +1,54 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
-const socketio = require("socket.io");
-const io = socketio(server, {
-  cors: {
-    origin: "*",
-  },
+const socketio = require('socket.io');
+
+const io = socketio(server);
+app.get("/*", function(req, res) {
+  res.write(`<h1>Hello socket</h1> ${PORT}`)
+  res.end
 });
-app.get("/*", function (req, res) {
-  res.write(`<h1>Hello socket</h1> ${PORT}`);
-  res.end;
+io.on('connection', socket => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  const users = [];
+  for (let [id, socket] of io.of('/').sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  io.emit('users', users);
+  console.log(users);
+  socket.broadcast.emit('user connected', {
+    userID: socket.id,
+    username: socket.username,
+  });
+  socket.on('private_message', ({content, to, timestamp}) => {
+    console.log('sent,recieve', content, to);
+    socket.to(to).emit('private_message', {
+      content,
+      from: socket.id,
+      time: `${timestamp.hour}:${timestamp.mins}`,
+    });
+  });
+ 
+  socket.on('disconnect', () => {
+    socket.disconnect();
+    console.log('🔥: A user disconnected');
+  });
 });
-io.on("connection", (socket) => {
-  console.log("a user connected :D");
-  socket.on("message", (message) => {
-    console.log(message);
-    io.emit("message", message);
-    socket.emit("message", message);
-  });
-  socket.on("delete", (message) => {
-    const obj = JSON.stringify(message);
-    io.emit("delete", `${obj}`);
-    socket.emit("delete", `${obj}`);
-    console.log(message);
-  });
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-  });
+
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  console.log(socket.handshake.auth.username, 'server');
+
+  if (!username) {
+    return next(new Error('invalid username'));
+  }
+  socket.username = username;
+  next();
 });
 
 const PORT = process.env.PORT || 3000;
