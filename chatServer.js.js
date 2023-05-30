8172235c3efc5097fce30345433cd96de91e74a2
Ahmed Ali <61229648+itxahmedali@users.noreply.github.com>
2023-05-30 05:47:46 -0700

    updating socket code for like

--- chatServer.js.js
+++ chatServer.js.js
@@ -1,67 +1,66 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 
 const io = socketio(server);
-app.get("/*", function(req, res) {
-  res.write(`<h1>Hello socket</h1> ${PORT}`)
-  res.end
+app.get("/*", function (req, res) {
+  res.write(`<h1>Hello socket</h1> ${PORT}`);
+  res.end;
 });
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
   const users = [];
-  for (let [id, socket] of io.of('/').sockets) {
+  for (let [id, socket] of io.of("/").sockets) {
     users.push({
       userID: id,
       username: socket.username,
     });
   }
-  io.emit('users', users);
+  io.emit("users", users);
   console.log(users);
-  socket.broadcast.emit('user connected', {
+  socket.broadcast.emit("user connected", {
     userID: socket.id,
     username: socket.username,
   });
-  socket.on('private_message', ({content, to, timestamp}) => {
-    console.log('sent,recieve', content, to);
-    socket.to(to).emit('private_message', {
+  socket.on("private_message", ({ content, to, timestamp }) => {
+    console.log("sent,recieve", content, to);
+    socket.to(to).emit("private_message", {
       content,
       from: socket.id,
       time: `${timestamp.hour}:${timestamp.mins}`,
     });
   });
-    socket.on('like', ({postUserId, myId}) => {
-      socket.emit('like', {
-        postUserId:postUserId,
-        myId: myId,
-      });
-      console.log('sent,recieve', postUserId, myId);
+  socket.on("like", ({ postId, postUserId, myId }) => {
+    io.emit("like", {
+      postId:postId,
+      postUserId: postUserId,
+      myId: myId
+    });
+    console.log("sent,recieve", postUserId, myId);
   });
- 
- 
-   socket.on('disconnect', () => {
+
+  socket.on("disconnect", () => {
     socket.disconnect();
     const users = [];
-    for (let [id, socket] of io.of('/').sockets) {
+    for (let [id, socket] of io.of("/").sockets) {
       users.push({
         userID: id,
         username: socket.username,
       });
     }
- io.emit('user-disconnected', users);
-    console.log('🔥: A user disconnected', users);
+    io.emit("user-disconnected", users);
+    console.log("🔥: A user disconnected", users);
   });
 });
 
-
 io.use((socket, next) => {
   const username = socket.handshake.auth.username;
-  console.log(socket.handshake.auth.username, 'server');
+  console.log(socket.handshake.auth.username, "server");
 
   if (!username) {
-    return next(new Error('invalid username'));
+    return next(new Error("invalid username"));
   }
   socket.username = username;
   next();
